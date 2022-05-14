b23629abe4bc2059162b7e151b02d9b8cc4eebf0
Brian Long <brian.long@fmadata.com>
2022-05-14 08:47:37 -0600

    Adds comments

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Solana Validator Metadata Tags &amp; Descriptions contains a list of commonly support tags and descriptions. The tags allow a validator to quickly disclose metadata that will be displayed on participating dashboards.
 
 See solana-validator-tags.[json,yml] for details.
+
+Use `ruby validate_tags.rb` for a quick file format tester.
--- validate_tags.rb
+++ validate_tags.rb
@@ -1,6 +1,6 @@
 # This is a quick & dirty script to validate the format of the included files.
-# Run like `ruby validate_tags.rb` and you should see '.' for successfully
-# parsed files. 'F' or an error means that something blew up.
+# Run `ruby validate_tags.rb` and you should see '.' for successfully parsed
+# files. 'F' or an error means that something blew up.
 
 require 'yaml'
 require 'json'
